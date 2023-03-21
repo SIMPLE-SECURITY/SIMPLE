f34df8f90725304323ab570318f5e46b4ed73741
tlsgusdn1107 <ashin2022@gmail.com>
2023-03-21 09:01:53 -0400

    utils update

--- Simple/Utils/Constants.swift
+++ Simple/Utils/Constants.swift
@@ -7,8 +7,6 @@
 
 import Firebase
 
-// all need to be in cloud; for later
-
 let polices =
 
 // emails specially allowed to have police account (police email domains are listed in "policeEmailDomains" variable)
